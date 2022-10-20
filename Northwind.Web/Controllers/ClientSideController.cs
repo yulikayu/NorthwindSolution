042fab3dcdf8ce3dd2d3dfd41f86f7bb733968fe
yulikayu <ayuyuli028@gmail.com>
2022-10-20 17:00:23 +0700

    JavaScript

---
+++ Northwind.Web/Controllers/ClientSideController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Web.Controllers
+{
+    public class ClientSideController : Controller
+    {
+        public IActionResult IndexJs()
+        {
+            return View();
+        }
+        public IActionResult IndexJQuery()
+        {
+            return View();
+        }
+    }
+}
